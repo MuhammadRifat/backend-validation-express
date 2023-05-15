d5d7fc44eb5b23048a247ccf13b71afbbccd8c4a
Rifat <rifat@ussbdpos.com>
2023-05-15 18:07:21 +0600

    initial version release

---
+++ lib/index.d.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+import Joi from "joi";
+interface IValidationSchema {
+    body?: Joi.AnySchema<any>;
+    params?: Joi.AnySchema<any>;
+    query?: Joi.AnySchema<any>;
+    headers?: Joi.AnySchema<any>;
+}
+declare const Validator: (validationSchema: IValidationSchema) => (req: Request, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
+export default Validator;
